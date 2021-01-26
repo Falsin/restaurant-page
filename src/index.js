e2b6216d58df1c4d3d5ddce2cae65243fb9a5750
Falsin <greenldend@gmail.com>
2021-01-26 20:04:34 +0700

    setting slidebar

--- src/index.js
+++ src/index.js
@@ -11,9 +11,31 @@ const nav = createELem(headerBox, 'nav');
 const img = createELem(nav, 'img');
 img.src = 'images/nav.svg';
 
-
 const slideBar = createELem(body, 'div');
-slideBar.id = 'slideBar'
+slideBar.id = 'slideBar';
+
+let arrayBoxes = [];
+let arrayLabels = [];
+
+for (let i = 0; i < 9; i++) {
+  arrayBoxes.push(createELem(slideBar, 'input'));
+  arrayBoxes[i].type = 'checkbox';
+  arrayBoxes[i].id = `${i}`;
+}
+
+const slide = createELem(slideBar, 'div');
+slide.id = 'slide';
+const picture = createELem(slide, 'img');
+picture.src = 'images/meat.jpg'
+
+const labels = createELem(slide, 'div');
+labels.id = 'labels';
+
+
+for (let i = 0; i < 9; i++) {
+  arrayLabels.push(createELem(labels, 'label'));
+  arrayLabels[i].setAttribute('for', `${i}`);
+}
 
 const footer = createELem(body, 'footer');
 
