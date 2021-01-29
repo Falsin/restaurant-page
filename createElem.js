2587e1577a39b60438a6a39f80520303d2d9808d
Falsin <greenldend@gmail.com>
2021-01-29 22:44:52 +0700

    added media requests

--- createElem.js
+++
@@ -1,9 +0,0 @@
-function createELem(parentElem, currentElem, ...arrayAttr) {	
-  let elem = document.createElement(currentElem);	
-  parentElem.appendChild(elem);	
-  arrayAttr = arrayAttr.map(item => item.split(':'))
-  arrayAttr.forEach(item => elem.setAttribute(`${item[0]}`, `${item[1]}`))
-  return elem;	
-}
-
-export {createELem};
