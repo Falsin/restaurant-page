512fec426406296172ac22c7854eec7b21fa21d1
Falsin <greenldend@gmail.com>
2021-01-31 12:51:34 +0700

    moved calcIndent, createMobileContainer and createLaptopContainer functions to the load module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import {createELem} from './createElem';
+import {calcIndent, body, header, slideBar, footer} from './load';
 
-const body = document.querySelector('body');
-const header = createELem(body, 'header');
 const headerBox = createELem(header, 'div', 'class:container');
 const logo = createELem(headerBox, 'div', 'id:logo');
 
@@ -10,7 +9,6 @@ logo.textContent = 'Good Food'
 const nav = createELem(headerBox, 'nav');
 const img = createELem(nav, 'img', 'src:images/nav.svg');
 
-const slideBar = createELem(body, 'div', 'id:slideBar');
 
 let arrayBoxes = [];
 let arrayLabels = [];
@@ -72,93 +70,8 @@ arrayLabels.forEach((item, id) => {
   })
 })
 
-const footer = createELem(body, 'footer');
+window.addEventListener("resize", () => calcIndent());
 
-window.addEventListener("resize", () => {
-  calcIndent();
-});
+window.onload = () => calcIndent();
 
-window.onload = () => {
-  createMobileContainer();
-  createLaptopContainer();
-  calcIndent();
-};
-
-function calcIndent() {
-  const windowHeight = document.documentElement.clientHeight;
-  const windowWidth = document.documentElement.clientWidth;
-
-  if(windowHeight < windowWidth && windowWidth >= 700 && windowWidth < 1000) {
-    const minSize = Math.min(windowHeight, windowWidth);
-
-    const btnDiv = document.querySelector('.btnDiv');
-    const computedStyle = getComputedStyle(btnDiv);
-    const btnSize = parseInt(computedStyle.height); 
-    const relativeSize = btnSize * 100 / minSize;
-
-    labels.style.bottom = `${relativeSize + 5}vmin`;
-    footer.style.paddingBottom = `${relativeSize + 5}vmin`;
-  } else if (windowHeight < windowWidth && windowWidth > 1000) {
-    const minSize = Math.min(windowHeight, windowWidth);
-
-    const computedStyle = getComputedStyle(footer);
-    const footerSize = parseInt(computedStyle.height); 
-    const relativeSize = footerSize * 100 / minSize;
-    labels.style.bottom = `${relativeSize + 5}vmin`;
-  } else {
-    labels.style.bottom = `5vmin`;
-  }
-}
-
-function createMobileContainer() {
-  const footerBox = createELem(footer, 'div', 'class:container');
-  const socialNetworks = createELem(footerBox, 'div', 'class:socialNetworks');
-
-  const facebookDiv = createELem(socialNetworks, 'div');
-  const facebookImg = createELem(facebookDiv, 'img', 'src:images/facebook.svg');
-
-  const twitterDiv = createELem(socialNetworks, 'div');
-  const twitterImg = createELem(twitterDiv, 'img', 'src:images/twitter.svg');
-
-  const instagramDiv = createELem(socialNetworks, 'div');
-  const instagramImg = createELem(instagramDiv, 'img', 'src:images/instagram.svg');
-
-  const adress = createELem(footerBox, 'div', 'class:adress');
-  adress.innerHTML = `© 2021 Good Food | <br> 1112 Parker St, Berkeley, CA 94702 | <br> (510) 024 — 377`;
-
-  const btnDiv = createELem(footerBox, 'div', 'class:btnDiv');
-  console.log(btnDiv);
-  const orderBtn = createELem(btnDiv, 'input', 'type:button', 'class:order', 'value:order online');
-
-  const author = createELem(footerBox, 'p', 'class:author');
-  const link = createELem(author, 'a');
-  link.textContent = 'Made by Falsin'
-  link.href = 'https://github.com/Falsin';
-}
-
-function createLaptopContainer() {
-  const windowHeight = document.documentElement.clientHeight;
-  const windowWidth = document.documentElement.clientWidth;
-
-  const footerBox = createELem(footer, 'div', 'class:laptopContainer');
-  const box = createELem(footerBox, 'div');
-
-  const adress = createELem(box, 'div', 'class:adress');
-  adress.innerHTML = `© 2021 Good Food | 1112 Parker St, Berkeley, CA 94702 | (510) 024 — 377`;
-
-  const socialNetworks = createELem(box, 'div', 'class:socialNetworks');
-
-  const facebookDiv = createELem(socialNetworks, 'div');
-  const facebookImg = createELem(facebookDiv, 'img', 'src:images/facebook.svg');
-
-  const twitterDiv = createELem(socialNetworks, 'div');
-  const twitterImg = createELem(twitterDiv, 'img', 'src:images/twitter.svg');
-
-  const instagramDiv = createELem(socialNetworks, 'div');
-  const instagramImg = createELem(instagramDiv, 'img', 'src:images/instagram.svg');
-
-  const author = createELem(footerBox, 'p', 'class:author');
-  const link = createELem(author, 'a');
-  link.textContent = 'Made by Falsin'
-  link.href = 'https://github.com/Falsin';
-}
+export {footer};
